a6249beacc6aa8de5db6aa1568780fc1c95df862
Jon Sampedro <jon.sampedro@bc3.net>
2024-03-11 17:03:30 +0100

    change website

--- R/rfasst.R
+++ R/rfasst.R
@@ -2,7 +2,7 @@
 #'
 #' \itemize{
 #' \item Github: https://github.com/JGCRI/rfasst
-#' \item Webpage: https://jgcri.github.io/rfasst/}
+#' \item Webpage: https://bc3lc.github.io/rfasst/}
 #'
 #' @docType package
 #' @name rfasst
